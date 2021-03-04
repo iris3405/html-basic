3be1f832cacdd69900d6abaae2a47c5d57e1b65a
iris3405 <78770213+iris3405@users.noreply.github.com>
2021-03-04 17:32:57 +0900

    Add files via upload

---
+++ lecture05.js
@@ -0,0 +1,15 @@
+// var a = 100, b = 3.14;
+// var c = "안녕하세요", d = "a";
+// var e = true, f = false;
+
+// console.log(a, typeof(a));
+// console.log(c, typeof(c));
+// console.log(e, typeof(e));
+
+var n = 1;
+var b = true;
+var s = "문자열입니다."
+
+console.log(n, typeof(n));
+console.log(b, typeof(b)); 
+console.log(s, typeof(s));
